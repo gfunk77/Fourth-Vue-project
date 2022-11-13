a0d0c9aff95b9de662bf50acff8fe9fa1e9371a6
gfunk77 <faraci77@gmail.com>
2022-11-13 08:56:24 -0500

    applied styles to box 1 and 2

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 const app = Vue.createApp({
   data() {
-    return {};
+    return {
+      box1: false,
+      box2: false,
+      box3: false,
+      box4: false,
+      box5: false,
+    };
   },
-  methods: {},
   computed: {},
+  methods: {
+    boxSelected(box) {
+      if (box === 1) {
+        this.box1 = !this.box1;
+      } else if (box === 2) {
+        this.box2 = !this.box2;
+      } else if (box === 3) {
+        this.box3 = !this.box3;
+      } else if (box === 4) {
+        this.box4 = !this.box4;
+      } else if (box === 5) {
+        this.box5 = !this.box5;
+      }
+    },
+  },
 });
 app.mount("#styling");
